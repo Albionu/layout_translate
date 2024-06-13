fd6c7dd6d83dad1c52a2f0ac3b0e44aa2837c0a1
Albionu <nikit.babaev2012@outlook.com>
2024-06-13 20:40:41 +0400

    init commit

--- index.php
+++ index.php
@@ -6,17 +6,17 @@ var_dump(dirname(__FILE__));
 
 require_once './vendor/autoload.php';
 
-use Telegram\Bot\Api;
-
-$telegram = new Api('7402378731:AAF_-wl_byFnbHWCS3-f-OzyfJkBrrHUvDM');
-//$result = $telegram->getUpdates();
-
-$id = $telegram->getChat()->id;
-
-function ruToEnLayout($text) : string {
-    $ru = ['й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю'];
-    $en = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.'];
-    return str_replace($ru, $en, $text);
-}
-
-$telegram->sendMessage(['id' => $id,'text' => 'text']);
+//use Telegram\Bot\Api;
+//
+//$telegram = new Api('7402378731:AAF_-wl_byFnbHWCS3-f-OzyfJkBrrHUvDM');
+////$result = $telegram->getUpdates();
+//
+//$id = $telegram->getChat()->id;
+//
+//function ruToEnLayout($text) : string {
+//    $ru = ['й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю'];
+//    $en = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.'];
+//    return str_replace($ru, $en, $text);
+//}
+//
+//$telegram->sendMessage(['id' => $id,'text' => 'text']);
