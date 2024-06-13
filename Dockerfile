aea4847a20dc364c4621af561565fd6f3c2c0655
Albionu <nikit.babaev2012@outlook.com>
2024-06-13 22:23:30 +0400

    init commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 FROM php:8.3-apache
+RUN rm -rf vendor
+RUN composer install --no-cache
+RUN composer dump-autoload
 COPY . /var/www/html
 WORKDIR /var/www/html
 EXPOSE 80
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '1d8945a555c38a4f7cd832eb5c32249fee5cebaf',
+        'reference' => 'a0cb2d5b82a6086769ad7e3d456f77bc6d446352',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '1d8945a555c38a4f7cd832eb5c32249fee5cebaf',
+            'reference' => 'a0cb2d5b82a6086769ad7e3d456f77bc6d446352',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
