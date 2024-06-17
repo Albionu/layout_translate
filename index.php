978349c09c0c568dab98ef926ec4b651808b512d
Albionu <nikit.babaev2012@outlook.com>
2024-06-18 01:57:30 +0400

    init commit

--- index.php
+++ index.php
@@ -27,13 +27,14 @@ try {
       'text' => ruToEnLayout($text, $is_forwarded),
     ]);
     
-    file_put_contents('output.txt', "Message sent successfully\n", FILE_APPEND);
+    //file_put_contents('output.txt', "Message sent successfully\n", FILE_APPEND);
 } catch (\Telegram\Bot\Exceptions\TelegramSDKException $e) {
-    file_put_contents('output.txt', "Error: " . 'not sent' . "\n", FILE_APPEND);
+    //file_put_contents('output.txt', "Error: " . 'not sent' . "\n", FILE_APPEND);
 } catch (Exception $e) {
-    file_put_contents('output.txt', "Error: " . 'trash' . "\n", FILE_APPEND);
+    //file_put_contents('output.txt', "Error: " . 'trash' . "\n", FILE_APPEND);
 }
-file_put_contents('output.txt', "Request getten " . rand(0, PHP_INT_MAX) . "\n", FILE_APPEND);
+
+//file_put_contents('output.txt', "Request getten " . rand(0, PHP_INT_MAX) . "\n", FILE_APPEND);
 
 function ruToEnLayout(string $text, bool $is_forwarded = false) : string {
     $ru = ['й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю'];
