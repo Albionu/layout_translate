0dd3024a23381c2d7e8618436efe9747ae8fab4a
Albionu <nikit.babaev2012@outlook.com>
2024-06-18 17:57:32 +0400

    update algo and maps

--- index.php
+++ index.php
@@ -4,60 +4,47 @@ require_once  "vendor/autoload.php";
 
 use Telegram\Bot\Api;
 
-$telegram = new Api('7402378731:AAF_-wl_byFnbHWCS3-f-OzyfJkBrrHUvDM');
+$telegram = new Api(token: '7402378731:AAF_-wl_byFnbHWCS3-f-OzyfJkBrrHUvDM');
 
-try {
-    $updates = $telegram->getWebhookUpdate();
-    
-    $message = $updates->getMessage();
-    
-    $chat = $message->get('chat');
-    
-    $text = $message->get('text');
-    
-    $id = $chat->id;
-    
-    $is_forwarded = false;
-    
-    if ($text == '/start') {startFunc($telegram, $id); die();}
-    
-    if($message->has('forward_origin'))
-        $is_forwarded = true;
-    
-    $MAP = [
-      '`' => 'ё','q' => 'й','w' => 'ц','e' => 'у','r' => 'к','t' => 'е','y' => 'н','u' => 'г','i' => 'ш','o' => 'щ','p' => 'з','[' => 'х',']' => 'ъ',
-      'a' => 'ф','s' => 'ы','d' => 'в','f' => 'а','g' => 'п','h' => 'р','j' => 'о','k' => 'л','l' => 'д',';' => 'ж',"'" => "э",
-      'z' => 'я','x' => 'ч','c' => 'с','v' => 'м','b' => 'и','n' => 'т','m' => 'ь',',' => 'б','.' => 'ю','/' => '.',
-      '~' => 'Ё','Q' => 'Ё','W' => 'Ц','E'=>'У','R'=>'К','T'=>'Е','Y'=>'Н','U'=>'Г','I'=>'Ш','O'=>'Щ','P'=>'З','{'=>'Х','}'=>'Ъ',
-      'A'=>'Ф','S'=>'Ы','D'=>'В','F'=>'А','G'=>'П','H'=>'Р','J'=>'О','K'=>'Л','L'=>'Д',':'=>'Ж','"'=>'Э',
-      'Z'=>'Я','X'=>'Ч','C'=>'С','V'=>'М','B'=>'И','N'=>'Т','M'=>'Ь','<'=>'Б','>'=>'Ю','?'=>',',
-    ];
-    
-    if ($is_forwarded)
-        $MAP = array_flip($MAP);
-    
-    $response = $telegram->sendMessage([
-      'chat_id' => $id,
-      'text' => ruToEnLayout($text, $MAP),
-    ]);
-    
-    file_put_contents('output.txt', "Message sent successfully\n", FILE_APPEND);
-} catch (\Telegram\Bot\Exceptions\TelegramSDKException $e) {
-    file_put_contents('output.txt', "Error: " . 'not sent' . "\n", FILE_APPEND);
-} catch (Exception $e) {
-    file_put_contents('output.txt', "Error: " . 'trash' . "\n", FILE_APPEND);
-}
+$updates = $telegram->getWebhookUpdate();
 
-file_put_contents('output.txt', "Request getten " . rand(0, PHP_INT_MAX) . "\n", FILE_APPEND);
+$message = $updates->getMessage();
 
-function ruToEnLayout (string $text, array $map) : string {
-    $res = '';
-    
-    foreach (mb_str_split($text) as $char)
-        $res .= $map[$char];
-    
-    return $res;
-}
+$chat = $message->get('chat');
+
+$text = $message->get('text');
+
+$id = $chat->id;
+
+$is_forwarded = false;
+
+if ($text == '/start') {startFunc($telegram, $id); die();}
+
+if($message->has('forward_origin'))
+    $is_forwarded = true;
+
+$MAP = [
+  '`' => 'ё','q' => 'й','w' => 'ц','e' => 'у','r' => 'к','t' => 'е','y' => 'н','u' => 'г','i' => 'ш','o' => 'щ','p' => 'з','[' => 'х',']' => 'ъ',
+  'a' => 'ф','s' => 'ы','d' => 'в','f' => 'а','g' => 'п','h' => 'р','j' => 'о','k' => 'л','l' => 'д',';' => 'ж',"'" => "э",
+  'z' => 'я','x' => 'ч','c' => 'с','v' => 'м','b' => 'и','n' => 'т','m' => 'ь',',' => 'б','.' => 'ю','/' => '.',
+  '~' => 'Ё','Q' => 'Ё','W' => 'Ц','E' => 'У','R' => 'К','T' => 'Е','Y' => 'Н','U' => 'Г','I' => 'Ш','O' => 'Щ','P' => 'З','{' => 'Х','}' => 'Ъ',
+  'A' => 'Ф','S' => 'Ы','D' => 'В','F' => 'А','G' => 'П','H' => 'Р','J' => 'О','K' => 'Л','L' => 'Д',':' => 'Ж','"' => 'Э',
+  'Z' => 'Я','X' => 'Ч','C' => 'С','V' => 'М','B' => 'И','N' => 'Т','M' => 'Ь','<' => 'Б','>' => 'Ю','?' => ',',
+];
+
+if ($is_forwarded)
+    $MAP = array_flip($MAP);
+
+$res = '';
+
+foreach (mb_str_split($text) as $char)
+    $res .= $MAP[$char];
+
+$response = $telegram->sendMessage([
+  'chat_id' => $id,
+  'text' => $res,
+]);
+
 
 function startFunc (object $telegram, int $ChatId) : void {
     $telegram->sendMessage([
