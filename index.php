b3a7a6f32a257dc8eec7c30a98ebe40174004b92
nikit <nikit.babaev2012@outlook.com>
2025-02-19 23:56:15 +0400

    code refactoring

--- index.php
+++ index.php
@@ -6,9 +6,7 @@ use Telegram\Bot\Api;
 
 $telegram = new Api(token: '7402378731:AAF_-wl_byFnbHWCS3-f-OzyfJkBrrHUvDM');
 
-$updates = $telegram->getWebhookUpdate();
-
-$message = $updates->getMessage();
+$message = $telegram->getWebhookUpdate()->getMessage();
 
 $chat = $message->get('chat');
 
@@ -16,24 +14,12 @@ $text = $message->get('text');
 
 $id = $chat->id;
 
-$is_forwarded = false;
-
 if ($text == '/start') {startFunc($telegram, $id); die();}
-
-if($message->has('forward_origin'))
-    $is_forwarded = true;
-
-$res = 'test';
-
-if ($is_forwarded)
-     $res = switcher_ru($text);
-else $res = switcher_en($text);
 
 $response = $telegram->sendMessage([
   'chat_id' => $id,
-  'text' => $res,
+  'text' => switcher($text, $message->has('forward_origin')),
 ]);
-
 
 function startFunc (object $telegram, int $ChatId) : void {
     $telegram->sendMessage([
@@ -42,50 +28,27 @@ function startFunc (object $telegram, int $ChatId) : void {
     ]);
 }
 
-function switcher_ru($value)
+function switcher(string $text = "", bool $reverse = false) : string
 {
-    $converter = array(
-      'f' => 'а',	',' => 'б',	'd' => 'в',	'u' => 'г',	'l' => 'д',	't' => 'е',	'`' => 'ё',
-      ';' => 'ж',	'p' => 'з',	'b' => 'и',	'q' => 'й',	'r' => 'к',	'k' => 'л',	'v' => 'м',
-      'y' => 'н',	'j' => 'о',	'g' => 'п',	'h' => 'р',	'c' => 'с',	'n' => 'т',	'e' => 'у',
-      'a' => 'ф',	'[' => 'х',	'w' => 'ц',	'x' => 'ч',	'i' => 'ш',	'o' => 'щ',	'm' => 'ь',
-      's' => 'ы',	']' => 'ъ',	"'" => "э",	'.' => 'ю',	'z' => 'я',
-      
-      'F' => 'А',	'<' => 'Б',	'D' => 'В',	'U' => 'Г',	'L' => 'Д',	'T' => 'Е',	'~' => 'Ё',
-      ':' => 'Ж',	'P' => 'З',	'B' => 'И',	'Q' => 'Й',	'R' => 'К',	'K' => 'Л',	'V' => 'М',
-      'Y' => 'Н',	'J' => 'О',	'G' => 'П',	'H' => 'Р',	'C' => 'С',	'N' => 'Т',	'E' => 'У',
-      'A' => 'Ф',	'{' => 'Х',	'W' => 'Ц',	'X' => 'Ч',	'I' => 'Ш',	'O' => 'Щ',	'M' => 'Ь',
-      'S' => 'Ы',	'}' => 'Ъ',	'"' => 'Э',	'>' => 'Ю',	'Z' => 'Я',
-      
-      '@' => '"',	'#' => '№',	'$' => ';',	'^' => ':',	'&' => '?',	'/' => '.',	'?' => ',',
-    );
-    
-    $value = strtr($value, $converter);
-    return $value;
+    $converter = [
+      'f' => 'а',',' => 'б','d' => 'в',	'u' => 'г',	'l' => 'д',	't' => 'е',	'`' => 'ё',
+      ';' => 'ж','p' => 'з','b' => 'и',	'q' => 'й',	'r' => 'к',	'k' => 'л',	'v' => 'м',
+      'y' => 'н','j' => 'о','g' => 'п',	'h' => 'р',	'c' => 'с',	'n' => 'т',	'e' => 'у',
+      'a' => 'ф','[' => 'х','w' => 'ц',	'x' => 'ч',	'i' => 'ш',	'o' => 'щ',	'm' => 'ь',
+      's' => 'ы',']' => 'ъ',"'" => "э",	'.' => 'ю',	'z' => 'я',
+
+      'F' => 'А','<' => 'Б','D' => 'В',	'U' => 'Г',	'L' => 'Д',	'T' => 'Е',	'~' => 'Ё',
+      ':' => 'Ж','P' => 'З','B' => 'И',	'Q' => 'Й',	'R' => 'К',	'K' => 'Л',	'V' => 'М',
+      'Y' => 'Н','J' => 'О','G' => 'П',	'H' => 'Р',	'C' => 'С',	'N' => 'Т',	'E' => 'У',
+      'A' => 'Ф','{' => 'Х','W' => 'Ц',	'X' => 'Ч',	'I' => 'Ш',	'O' => 'Щ',	'M' => 'Ь',
+      'S' => 'Ы','}' => 'Ъ','"' => 'Э',	'>' => 'Ю',	'Z' => 'Я',
+
+      '@' => '"','#' => '№','$' => ';',	'^' => ':',	'&' => '?',	'/' => '.',	'?' => ',',
+    ];
+
+    if ($reverse) $converter = array_flip($converter);
+
+    return strtr($text, $converter);
 }
-
-function switcher_en($value)
-{
-    $converter = array(
-      'а' => 'f',	'б' => ',',	'в' => 'd',	'г' => 'u',	'д' => 'l',	'е' => 't',	'ё' => '`',
-      'ж' => ';',	'з' => 'p',	'и' => 'b',	'й' => 'q',	'к' => 'r',	'л' => 'k',	'м' => 'v',
-      'н' => 'y',	'о' => 'j',	'п' => 'g',	'р' => 'h',	'с' => 'c',	'т' => 'n',	'у' => 'e',
-      'ф' => 'a',	'х' => '[',	'ц' => 'w',	'ч' => 'x',	'ш' => 'i',	'щ' => 'o',	'ь' => 'm',
-      'ы' => 's',	'ъ' => ']',	'э' => "'",	'ю' => '.',	'я' => 'z',
-      
-      'А' => 'F',	'Б' => '<',	'В' => 'D',	'Г' => 'U',	'Д' => 'L',	'Е' => 'T',	'Ё' => '~',
-      'Ж' => ':',	'З' => 'P',	'И' => 'B',	'Й' => 'Q',	'К' => 'R',	'Л' => 'K',	'М' => 'V',
-      'Н' => 'Y',	'О' => 'J',	'П' => 'G',	'Р' => 'H',	'С' => 'C',	'Т' => 'N',	'У' => 'E',
-      'Ф' => 'A',	'Х' => '{',	'Ц' => 'W',	'Ч' => 'X',	'Ш' => 'I',	'Щ' => 'O',	'Ь' => 'M',
-      'Ы' => 'S',	'Ъ' => '}',	'Э' => '"',	'Ю' => '>',	'Я' => 'Z',
-      
-      '"' => '@',	'№' => '#',	';' => '$',	':' => '^',	'?' => '&',	'.' => '/',	',' => '?',
-    );
-    
-    $value = strtr($value, $converter);
-    return $value;
-}
-
-
 
 die();
